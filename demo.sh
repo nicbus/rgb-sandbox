2982166837d006d5f57f5382d9b8d1ce3ff09277
Nicola Busanello <nicola.busanello@gmail.com>
2024-01-31 17:22:19 +0100

    accept the same transfer twice

--- demo.sh
+++ demo.sh
@@ -90,7 +90,7 @@ _wait_esplora_sync() {
         echo -n "."
         sleep 1
     done
-    echo "synced"
+    echo " done"
 }
 
 _gen_blocks() {
@@ -525,7 +525,10 @@ transfer_complete() {
     [ $DEBUG = 1 ] && _log "recipient unspents after transfer" && _list_unspent "$RCPT_WLT"
     _subtit "final balances"
     check_balance "$SEND_WLT" "$BLNC_SEND" "$NAME"
-    check_balance "$RCPT_WLT" "$BLNC_RCPT" "$NAME"
+    #check_balance "$RCPT_WLT" "$BLNC_RCPT" "$NAME"
+    local blnc_rcpt=$BLNC_RCPT
+    [ -n "$CUSTOM_BLNC_RCPT" ] && blnc_rcpt=$CUSTOM_BLNC_RCPT
+    check_balance "$RCPT_WLT" "$blnc_rcpt" "$NAME"
 }
 
 help() {
@@ -577,10 +580,10 @@ setup_rgb_clients
 _tit "issuing assets"
 get_issue_utxo
 issue_asset "usdt" "NIA"
-issue_asset "collectible" "CFA"
+#issue_asset "collectible" "CFA"
 _tit "checking asset balances after issuance"
 check_balance "issuer" "2000" "usdt"
-check_balance "issuer" "2000" "collectible"
+#check_balance "issuer" "2000" "collectible"
 
 # export/import asset
 _tit "exporting asset"
@@ -588,45 +591,58 @@ export_asset usdt
 _tit "importing asset to recipient 1"
 import_asset usdt rcpt1
 
-# TODO: re-introduce aborted transfer + 2nd asset (blank)
-# transfer loop:
-#   1. issuer -> rcpt 1 (spend issuance)
-#     1a. only initiate tranfer, don't complete (aborted transfer)
-#     1b. retry transfer (re-using invoice) and complete it
-#     1c. check asset balances
-#   2. issuer -> rcpt 1 (CFA)
-#   3. issuer -> rcpt 1 (spend change, using witness vout)
-#   4. rcpt 1 -> rcpt 2 (spend both received allocations)
-#   5. rcpt 2 -> issuer (close loop)
-#   6. issuer -> rcpt 1 (spend received back)
-#     6a. check asset balances
-_tit "transferring (spend issuance)"
+# save current issuer + chain data
+_tit "saving issuer + chain data"
+_subtit "stopping esplora services"
+for SRV in nginx tor socat websocket prerenderer electrs bitcoin; do
+    docker compose exec esplora bash -c "sv stop /etc/service/$SRV"
+done
+docker compose down
+_subtit "saving current data"
+rm -fr data0.1
+docker run --rm -v "$(pwd):/data" debian:bookworm bash -c "rm -rf /data/dataesplora.1"
+cp -a data0{,.1}
+docker run --rm -v "$(pwd):/data" debian:bookworm bash -c "cp -a /data/dataesplora{,.1}"
+_subtit "restarting services"
+docker compose up -d
+echo -n "waiting for esplora to have started"
+until docker compose logs esplora |grep -q 'Electrum RPC server running'; do
+    echo -n "."
+    sleep 1
+done
+echo " done"
+"${BCLI[@]}" loadwallet miner   # load bitcoind wallet
+_wait_esplora_sync
+
+# transfer a 1st time
+_tit "transferring (1st)"
 transfer_asset issuer/rcpt1 2000/0 100/1900 0 0 usdt
 
-_tit "checking issuer balances after the 1st transfer (blank transition)"
-check_balance issuer 1900 usdt
-check_balance issuer 2000 collectible
+# restore previous issuer data
+_tit "restoring issuer + chain data"
+_subtit "stopping esplora services"
+for SRV in nginx tor socat websocket prerenderer electrs bitcoin; do
+    docker compose exec esplora bash -c "sv stop /etc/service/$SRV"
+done
+docker compose down
+_subtit "restoring previous data"
+rm -fr data0
+docker run --rm -v "$(pwd)/:/data" debian:bookworm bash -c "rm -rf /data/dataesplora"
+mv data0{.1,}
+docker run --rm -v "$(pwd):/data" debian:bookworm bash -c "mv /data/dataesplora{.1,}"
+_subtit "restarting services"
+docker compose up -d
+echo -n "waiting for esplora to have started"
+until docker compose logs esplora |grep -q 'Electrum RPC server running'; do
+    echo -n "."
+    sleep 1
+done
+echo " done"
+"${BCLI[@]}" loadwallet miner   # load bitcoind wallet
+_wait_esplora_sync
 
-_tit "transferring (CFA)"
-transfer_asset issuer/rcpt1 2000/0 200/1800 0 0 collectible
-
-_tit "transferring (spend change, using witness vout)"
-transfer_asset issuer/rcpt1 1900/100 200/1700 1 0 usdt
-
-_tit "transferring (spend received)"
-transfer_asset rcpt1/rcpt2 300/0 150/150 0 0 usdt
-
-_tit "transferring (witness vout)"
-transfer_asset rcpt2/issuer 150/1700 100/50 1 0 usdt
-
-_tit "transferring (spend received back)"
-transfer_asset issuer/rcpt1 1800/150 50/1750 0 0 usdt
-
-_tit "checking final balances"
-check_balance issuer 1750 usdt
-check_balance rcpt1 200 usdt
-check_balance rcpt2 50 usdt
-check_balance issuer 1800 collectible
-check_balance rcpt1 200 collectible
-
-_tit "sandbox run finished"
+# make the same transfer a 2nd time, using the same invoic, and check it's accepted with no issues
+_tit "transferring (2nd)"
+CUSTOM_BLNC_RCPT=100    # custom final recipient balance, as it doesn't change with this transfer
+# 2000/100 (expected initial recipient balance 100) as rcpt1 already sees the previous allocation
+transfer_asset issuer/rcpt1 2000/100 100/1900 0 1 usdt
